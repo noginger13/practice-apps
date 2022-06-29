e8179e491253c6746217ed7bfbf79cc0be55cf8c
Michael Schoenecker <michael.schoenecker@gmail.com>
2022-06-29 17:27:35 -0400

    added connection to database, schema, and model skeletons

--- 1-glossary/server/db.js
+++ 1-glossary/server/db.js
@@ -1,6 +1,63 @@
+require("dotenv").config();
 const mongoose = require("mongoose");
 
-// 1. Use mongoose to establish a connection to MongoDB
-// 2. Set up any schema and models needed by the app
-// 3. Export the models
-// 4. Import the models into any modules that need them
+//Establish a connection to MongoDB
+mongoose.connect(`mongodb://localhost:2707/${process.env.DB_NAME}`)
+.then(() => {
+  console.log(`MongoDB connected to DB ${process.env.DB_NAME}`);
+})
+.catch((err) => {
+  console.error(`MongoDB was unable to conect to DB ${process.env.DB_NAME}`);
+});
+
+//Set up schema
+const glossarySchema = new mongoose.Schema({
+  word: String,
+  definition: String
+});
+
+const Glossary = mongoose.model(`${process.env.DB_NAME}`, glossarySchema);
+
+//Set up models
+
+//Add word to DB
+const add = (wordObj) => {
+  return Glossary
+  .findOneAndUpdate(
+    {
+      word: wordObj.word,
+      definition: wordObj.meanings.definitions[0].definition
+    },
+    {},
+    {upsert: true}
+  );
+};
+
+//TODO: Find and replace
+// const findWord = () => {
+//   return Glossary
+//   .find({word});
+// };
+
+//Removes word from DB
+const remove = (word) => {
+  return Glossary
+  .deleteOne({word});
+}
+
+//Searches DB for word like term
+const search = (term) => {
+  return Glossary
+  .find({word: `/${term}/i`});
+}
+
+
+//EXPORT MODELS
+modules.export = {
+  add,
+  remove,
+  search
+}
+
+// TODO: 4. Import the models into any modules that need them
+
