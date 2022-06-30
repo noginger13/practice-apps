3c10321f0e959a900fdb38d6d481f410918568fe
Michael Schoenecker <michael.schoenecker@gmail.com>
2022-06-29 20:03:45 -0400

    added pseudocode for the routes and connected to the database

--- 1-glossary/server/index.js
+++ 1-glossary/server/index.js
@@ -2,18 +2,42 @@ require("dotenv").config();
 const express = require("express");
 const path = require("path");
 
+const db = require("./db.js");
+
+
 const app = express();
 
+app.use(express.json());
 // Serves up all static and generated assets in ../client/dist.
 app.use(express.static(path.join(__dirname, "../client/dist")));
 
-/**** 
- * 
- * 
- * Other routes here....
- *
- * 
- */
+
+//ROUTES
+
+//Initial
+
+app.get('/init', (req, res) => {
+   db.add({
+    word: 'Add a word to the glossary!',
+    definition: 'You can delete this sample with the buttons.'
+  })
+  .catch((err) => console.error(err))
+})
+
+
+
+//Search
+
+
+//Add
+
+
+//Edit
+
+
+//Delete
+
+
 
 app.listen(process.env.PORT);
 console.log(`Listening at http://localhost:${process.env.PORT}`);
